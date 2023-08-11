124f67e4de469f24ef53f0d0f82f2cef1b4fa971
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-11 22:20:19 +0600

    Arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,75 @@
+// JavaScript Arrays
+
+// Group of values
+
+// JavaScript arrays are written with square brackets.
+
+// Array items are separated by commas.
+
+
+
+// ========================================
+
+
+
+const car = ["Volvo", "BMW", "Audi", "Mercedes"];
+console.log(car[3]);
+
+
+
+// ========================================
+
+
+
+// Push
+// Add element to the end of the array
+// var add = [1, 2, 3, 4, 5, 6, 7, 8];
+// add.push(9);
+
+
+
+// ========================================
+
+
+
+// Pop
+// Remove element to the end of the array
+// var cut = [1, 2, 3, 4, 5, 6, 7, 8];
+// cut.pop();
+
+
+
+// ========================================
+
+
+
+// Splice
+// Splice remove value from any position
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.splice(3, 2);
+
+
+
+// ========================================
+
+
+
+// Shift
+// Shift remove 1 element from the start
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.shift();
+
+
+
+// ========================================
+
+
+
+// Unshift
+// Unshift add 1 element from the start
+// var arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.unshift(0);
+
+
+
+// ========================================
