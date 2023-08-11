3f79f7216bebfc663e9db13c8be999fbb83474d8
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-11 22:12:14 +0600

    Primitive

---
+++ Primitive Data Types/Primtive.js
@@ -0,0 +1,17 @@
+// Primitive Data Types in JavaScript
+
+// Primitive = number, string, boolean, null, undefined, symbol
+
+// Number = 123;
+// String = "Text";
+// Boolean = true / false;
+
+// Example
+
+var a = 50;
+var b = a; // b = a = 50
+a = 100;
+
+console.log(a); // Outputs 100
+
+console.log(b); // Outputs 50
