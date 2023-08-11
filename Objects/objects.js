b9a80233aafca821a0bed08a594bb3e9a4dedce8
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-11 22:20:44 +0600

    Objects

---
+++ Objects/objects.js
@@ -0,0 +1,36 @@
+// JavaScript Objects
+
+// JavaScript objects are written with curly braces {}.
+
+// Object properties are written as name: value pairs, separated by commas.
+
+
+
+// ========================================
+
+
+
+// Blank object
+// var a = {};
+
+
+
+// ========================================
+
+
+
+// Filled object
+var obj = {
+    name: "Kara",
+    country: "Kazak",
+    email: "kara@gmail.com",
+    age: 25,
+    contact: 123456789
+};
+obj.name = "Murat"; // Access and change the value
+console.log(obj);
+document.getElementById('demo').innerHTML = "Name: " + obj.name + "<br>" + "Age: " + obj.age + "<br>" + "Email: " + obj.email;
+
+
+
+// ========================================
