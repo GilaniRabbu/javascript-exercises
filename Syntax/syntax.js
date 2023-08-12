13f1a2d7ad048e178306ab4330b6576f95886597
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-12 18:01:33 +0600

    Syntax

---
+++ Syntax/syntax.js
@@ -0,0 +1,39 @@
+// var let const = Keyword
+
+// JavaScript is Case Sensitive
+
+let surname, surName;
+
+surname = "Kara";
+
+surName = "Max";
+
+document.write(surname);
+
+console.log(surName);
+
+
+
+// ========================================
+
+
+
+document.getElementById("demo").innerHTML = "Hello, World!";
+
+document.getElementById("demo").style.fontSize = "30px";
+
+document.getElementById("demo").style.fontFamily = "monospace";
+
+
+
+// ========================================
+
+
+
+window.alert('Welcome to Website');
+
+console.log('Hello World');
+
+
+
+// ========================================
