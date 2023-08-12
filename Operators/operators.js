f2a69998d2e3e8574979e26ab7d138e2149ff412
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-12 18:00:34 +0600

    Operators

---
+++ Operators/operators.js
@@ -0,0 +1,126 @@
+// JavaScript Operators
+
+// Types of JavaScript Operators
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+
+
+// ========================================
+
+
+
+// JavaScript Arithmetic Operators
+
+// Addition[+] Subtraction[-] Multiplication[*] Division[/]
+// Exponentiation[**] Increment[++] Decrement[--] Modulus[%]
+
+var a = 5;
+var b = (15 + 5) * a;
+console.log(b);
+
+var x = 5;
+x++;
+var z = x;
+console.log(x);
+
+
+
+// ========================================
+
+
+
+// JavaScript Assignment Operators
+
+// Assignment operators assign values to JavaScript variables.
+
+// [=] [+=] [-=] [*=] [/=] [%=] [**=]
+
+var x = 10;
+x += 10;
+console.log(x);
+
+
+
+// ========================================
+
+
+
+// JavaScript Comparison Operators
+
+// [==] - equal to
+// [===] - equal value and equal type
+// [!=] - not equal
+// [!==] - not equal value or not equal type
+// [>] - greater than
+// [<] - less than
+// [>=] - greater than or equal to
+// [<=] - less than or equal to
+// [?] - ternary operator
+
+
+
+// ========================================
+
+
+
+// JavaScript String Comparison
+
+var text = "Hello";
+var text1 = "World";
+var result = text < text1;
+console.log(result); // Output true
+
+
+
+// ========================================
+
+
+
+// Adding Strings and Numbers
+
+var a = 5 + 5;
+var b = "10 " + 10;
+var c = "Hello " + 100;
+console.log(a + " " + b + " " + c);
+
+
+
+// ========================================
+
+
+
+// JavaScript Logical Operators
+// [&&] - logical and
+// [||] - logical or
+// [!] - logical not
+
+// JavaScript Type Operators
+// [typeof] - Returns the type of a variable
+
+
+
+// ========================================
+
+
+
+// JavaScript Bitwise Operators
+
+// [&] - AND
+// [|] - OR
+// [~] - NOT
+// [^] - XOR
+// [<<] - left shift
+// [>>] - right shift
+// [>>>] - unsigned right shift
+
+
+
+// ========================================
