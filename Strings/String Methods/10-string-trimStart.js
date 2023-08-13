258a6079af72a2c99672b211b845f05717cfb7f9
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-14 00:20:56 +0600

    Strings

---
+++ Strings/String Methods/10-string-trimStart.js
@@ -0,0 +1,12 @@
+// JavaScript String trimStart()
+
+// The trimStart() method works like trim(),
+// But removes whitespace only from the start of a string.
+
+let text = "     Hello World!     ";
+
+let Text = text.trimStart();
+
+console.log(text.length); // Output 22
+
+console.log(Text.length); // Output 17
---
+++ Strings/String Methods/9-string-trim.js
@@ -0,0 +1,11 @@
+// JavaScript String trim()
+
+// The trim() method removes whitespace from both sides of a string
+
+let text = "     Hello World!     ";
+
+let Text = text.trim();
+
+console.log(text.length); // Output 22
+
+console.log(Text.length); // Output 12
