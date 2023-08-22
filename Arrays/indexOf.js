ae7174ddaeb180b9f41176a52e9cf3b170744797
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:03:29 +0600

    Arrays

---
+++ Arrays/indexOf.js
@@ -0,0 +1,13 @@
+// Array IndexOf
+
+// Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+
+// SearchElement — The value to locate in the array.
+
+// FromIndex — The array index at which to begin the search. If fromIndex is omitted, the search starts at index 0.
+
+// Showing results in Number format
+
+var numArray = [30, 32, 40, 55, 60, 80, 90];
+var result = numArray.indexOf(60);
+document.write(result);
