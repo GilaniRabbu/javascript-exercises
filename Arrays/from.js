ff49a045c1b99009c83d7c5027997145326a1c58
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:02:58 +0600

    Arrays

---
+++ Arrays/from.js
@@ -0,0 +1,9 @@
+// Array Form
+
+// Creates an array from an iterable object.
+
+// Iterable — An iterable object to convert to an array.
+
+var Title = 'Learn With Developer';
+var TitleArray = Array.from(Title);
+document.write(TitleArray[11]);
