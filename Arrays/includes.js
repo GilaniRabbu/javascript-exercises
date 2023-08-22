370dbf7207dc3fff2702e14bb721df274232685b
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:03:13 +0600

    Arrays

---
+++ Arrays/includes.js
@@ -0,0 +1,13 @@
+// Array Includes
+
+// Determines whether an array includes a certain element, returning true or false as appropriate.
+
+// SearchElement — The element to search for.
+
+// FromIndex — The position in this array at which to begin searching for searchElement.
+
+// Showing results in Boolean format
+
+var numArray = [30, 32, 40, 55, 60, 80, 90];
+var result = numArray.includes(70);
+document.write(result);
