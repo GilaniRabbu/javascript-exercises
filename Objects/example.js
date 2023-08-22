ebc4ca47fd4dbd30517f54a744f073bbd59cd7c7
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:56:03 +0600

    Objects

---
+++ Objects/example.js
@@ -0,0 +1,13 @@
+// Objects
+
+var PC = {
+    brand: 'HP',
+    screen: '15.6',
+    isLaptop: true,
+    isSSD: true,
+    ram: '8GB',
+    disk: '50GB'
+}
+document.write(PC['isLaptop'] + '<br>');
+document.write(PC.isSSD + '<br>');
+document.write(PC.ram);
