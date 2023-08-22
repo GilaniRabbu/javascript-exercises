e0175409f26c6d9b03eb092755e83a49b52e3f58
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 22:58:30 +0600

    Arrays

---
+++ Arrays/concat.js
@@ -0,0 +1,10 @@
+// Array Concat
+
+// Combines two or more arrays. This method returns a new array without modifying any existing arrays.
+
+// Items — Additional arrays and / or items to add to the end of the array.
+
+var arr1 = ["A,B,C"];
+var arr2 = ["D,E,F"];
+var arr3 = arr1.concat(arr2);
+document.write(arr3); // Output A,B,C,D,E,F
