41b30c1b40b5d9c0aa71078103326b763fd17f1b
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:03:44 +0600

    Arrays

---
+++ Arrays/sort-slice.js
@@ -0,0 +1,15 @@
+// Array Sort Slice
+
+// var numArray = [55, 60, 80, 30, 32, 40, 90];
+// numArray.sort();
+// document.write(numArray);
+
+// var numArray = [55, 60, 80, 30, 32, 40, 90];
+// numArray.sort().reverse();
+// document.write(numArray);
+
+// The JavaScript array slice() method extracts the part of the given array
+// and returns it. This method doesn't change the original array.
+var numArray = [55, 60, 80, 30, 32, 40, 90];
+var result = numArray.slice(3, 5);
+document.write(result);
