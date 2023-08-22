165ec8b7b3c9a00b755e6698fe35098e11879604
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-22 23:54:50 +0600

    Navigator

---
+++ Navigator/script.js
@@ -0,0 +1,32 @@
+// The Window Navigator Object
+
+// The navigator object contains information about the browser.
+
+// The navigator object is a property of the window object.
+
+var appCodeName = navigator.appCodeName;
+var appName = navigator.appName;
+var appVersion = navigator.appVersion;
+var cookieEnabled = navigator.cookieEnabled;
+var language = navigator.language;
+var userAgent = navigator.userAgent;
+var platform = navigator.platform;
+var product = navigator.product;
+
+document.write(appCodeName + "<br>");
+document.write(appName + "<br>");
+document.write(appVersion + "<br>");
+document.write(cookieEnabled + "<br>");
+document.write(language + "<br>");
+document.write(userAgent + "<br>");
+document.write(platform + "<br>");
+document.write(product + "<br>");
+
+// document.writeln("<br/>navigator.appCodeName: " + navigator.appCodeName);
+// document.writeln("<br/>navigator.appName: " + navigator.appName);
+// document.writeln("<br/>navigator.appVersion: " + navigator.appVersion);
+// document.writeln("<br/>navigator.cookieEnabled: " + navigator.cookieEnabled);
+// document.writeln("<br/>navigator.language: " + navigator.language);
+// document.writeln("<br/>navigator.userAgent: " + navigator.userAgent);
+// document.writeln("<br/>navigator.platform: " + navigator.platform);
+// document.writeln("<br/>navigator.onLine: " + navigator.onLine);
