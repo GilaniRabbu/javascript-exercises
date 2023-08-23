ee1f2f8fe862db71de7463fa2d828cfc656bf862
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-23 13:24:00 +0600

    Number

---
+++ Number/toExponential.js
@@ -0,0 +1,9 @@
+// The toExponential() Method
+
+// toExponential() returns a string, with a number rounded and written using exponential notation.
+
+let a = 8.555;
+
+document.write(
+    a.toExponential() + "<br>" + a.toExponential(2) + "<br>" + a.toExponential(7)
+)
---
+++ Number/toFixed.js
@@ -0,0 +1,9 @@
+// The toFixed() Method
+
+// toFixed() returns a string, with the number written with a specified number of decimals
+
+var num = 7.98549874552025;
+
+document.write(
+    num.toFixed(5)
+)
