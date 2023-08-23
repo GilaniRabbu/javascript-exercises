40585afeac9b62d64fdf6033d7f87a91cd6604bc
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-23 13:23:02 +0600

    Number

---
+++ Number/parseFloat.js
@@ -0,0 +1,11 @@
+// The Number.parseFloat() Method
+
+// Number.parseFloat() parses a string and returns a number.
+
+// If the number cannot be converted, NaN (Not a Number) is returned.
+
+var num = "7.98";
+
+document.write(
+    Number.parseFloat(num) + "<br>" + Number.parseFloat("10.33")
+)
