2b635482c0f08bb6e225730c1718b213011d478f
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-23 13:24:39 +0600

    Number

---
+++ Number/toPrecision.js
@@ -0,0 +1,9 @@
+// The toPrecision() Method
+
+// toPrecision() returns a string, with a number written with a specified length
+
+let a = 95.548975156;
+
+document.write(
+    a.toPrecision(2) + "<br>" + a.toPrecision(4) + "<br>" + a.toPrecision(6)
+)
---
+++ Number/toString.js
@@ -0,0 +1,9 @@
+// The toString() Method
+
+// The toString() method returns a number as a string.
+
+var num1 = 7.98549874552025;
+
+document.write(
+    num1.toString()
+)
